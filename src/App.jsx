fc313a19739585be17838f05ea8ae195f412dc93
sawonhossain34 <sawonhossain3491@gmail.com>
2023-06-07 10:01:38 +0600

    setup react and install tailwind and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     
+     <h2 className='text-5xl text-green-700 text font-bold'>Hello Learn Photography</h2>
+      
+      
+    </>
+  )
+}
+
+export default App
